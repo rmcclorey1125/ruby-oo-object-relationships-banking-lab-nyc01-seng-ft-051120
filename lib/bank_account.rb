570c97ac7469c62812a4232cecc3300a01adc2da
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-05-14 13:54:20 -0400

    first update

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 class BankAccount
+    attr_reader :name, :balance, :status
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        binding.pry
+        # @status = status
+    end
 
 end

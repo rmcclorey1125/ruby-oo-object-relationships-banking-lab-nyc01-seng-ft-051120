5491f2737c06973a7f86ac0b9cb5c859e1e1fdfe
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-05-14 14:16:41 -0400

    finish up to valid? method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,15 +1,31 @@
 require 'pry'
 
 class BankAccount
-    attr_reader :name, :balance, :status
+    attr_reader :name, :status
+    attr_accessor :balance, :status
 
     def initialize(name)
         @name = name
         @balance = 1000
-        binding.pry
+        @status = "open"
+        # binding.pry
         # @status = status
     end
 
+    def deposit(amount)
+      @balance += amount
+    end
 
-    
+    def display_balance
+      "Your balance is $#{@balance}."
+    end
+
+    def valid?
+      if @balance <= 0 || @status == "closed"
+        false
+      else 
+        true
+      end
+    end
+
 end

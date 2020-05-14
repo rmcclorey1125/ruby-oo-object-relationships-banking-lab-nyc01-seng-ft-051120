0887d29b720a01d5d8cc8e879c5a2d7fc239f88a
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-05-14 15:53:36 -0400

    finished

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 class Transfer
-  attr_reader :sender, :receiver, :amount, :status
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
 
   def initialize(sender, receiver, amount)
     @sender = sender
@@ -11,20 +12,36 @@ class Transfer
   end
 
   def valid?
-    if @sender.valid? && @receiver.valid?
-      true
-    else
-      false
-    end
+    @sender.valid? && @receiver.valid?
+
   end
 
   def execute_transaction
-    @sender.balance -= amount
-    @receiver.balance += amount
-    @status = "complete"
+      # binding.pry
+      if self.valid?  && sender.balance > amount && self.status == "pending"
+        @sender.balance -= amount
+        @receiver.balance += amount
+        @status = "complete"
+      else
+        self.status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
   end
 
+  # def execute_transaction
+  #   if  @status != "complete" && self.valid?
+  #     @sender.balance -= amount
+  #     @receiver.balance += amount
+  #     @status = "complete"
+  #   end
+  # end
 
-
+  def reverse_transfer
+    if  @status == "complete"
+      @status = "reversed"
+      @receiver.balance -= amount
+      @sender.balance += amount
+    end
+  end
 
 end
